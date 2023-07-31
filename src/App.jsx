88777bef2cb1e72101bc250de01acb1f5f090bee
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-08-01 03:25:58 +0530

    Got a Sidebar ,PHEWWWWWWW

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-// import './App.css'
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import { Stack } from "@mui/material";
+import {Typography} from '@mui/material'
 
-import Sidebar from './components/Sidebar/Sidebar.jsx'
-import Tool from './components/Tool/Tool.jsx'
 
+// import Sidebar from './components/Sidebar/Sidebar.jsx'
+// import Tool from './components/Tool/Tool.jsx'
 
-export default App = () => {
+import './App.css'
+import Side_bar_MockUp from './components/Sidebar/Side_bar_MockUp';
 
-    const [SelectedCategory, setSelectedCategory ] = useState('New');
+const App = () => {
+
+    const [selectedTool, setSelectedTool ] = useState('New');
+    
 
     return (
-        <>
-        <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-            <Sidebar selectedCategory={SelectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Stack sx={{flexDirection: { x: 'column', md: 'row'}}}>
+
+            <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+                <Side_bar_MockUp selectedTool={selectedTool} setSelectedTool={setSelectedTool} />
             </Box>
-        </>
+
+        </Stack>
     )
 }
+
+export default App;
--- src/components/Sidebar/Side_bar_MockUp.jsx
+++ src/components/Sidebar/Side_bar_MockUp.jsx
@@ -5,12 +5,24 @@ import { Stack } from "@mui/material";
 // This is that list we made in utils/constatnts.js
 // import { tools } from "../Tool/{import_an_array_of_Tools}";
 
-tools = [
-    {icon:"THIS IS THE ICON OF THE TOOL1", name: "THIS IS THE NAME OF THE TOOL1"},
-    {icon:"THIS IS THE ICON OF THE TOOL2", name: "THIS IS THE NAME OF THE TOOL2"},
-    {icon:"THIS IS THE ICON OF THE TOOL3", name: "THIS IS THE NAME OF THE TOOL3"},
-    {icon:"THIS IS THE ICON OF THE TOOL4", name: "THIS IS THE NAME OF THE TOOL4"},
-    {icon:"THIS IS THE ICON OF THE TOOL5", name: "THIS IS THE NAME OF THE TOOL5"},
+const tools = [
+    {icon:"ICON1 ", name: "NAME1"},
+    {icon:"ICON2 ", name: "NAME2"},
+    {icon:"ICON3 ", name: "NAME3"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON4 ", name: "NAME4"},
+    {icon:"ICON5 ", name: "NAME5"},
+    {icon:"ICON5 ", name: "NAME5"},
+    {icon:"ICON5 ", name: "NAME5"},
 ]
 
 /*
@@ -38,9 +50,9 @@ tools = [
 
 
 
-export default Component = ({ selectedTool, setSelectedTool }) => (
+const Side_bar_MockUp = ({ selectedTool, setSelectedTool }) => (
 
-    <Stack direction="row" sx={{overflowY: "auto",height: { sx: "auto", md: "95%" },flexDirection: { md: "column" },}}>
+    <Stack direction="row" sx={{overflowY: "auto",height: { sx: "auto", md: "95%" },flexDirection: { md: "column" },width: "200px"}}>
         <Box sx= {{backgroundColor: '#000'}}> 
 
         {tools.map((tool) => (
@@ -49,12 +61,12 @@ export default Component = ({ selectedTool, setSelectedTool }) => (
         <button
             classname="tool-btn"
             onclick={() => setSelectedTool(tool.name)}  
-            style={{background: tool.name === selectedTool && "#fc1503",color: "white",}}
+            style={{background: tool.name === selectedTool && "#fc1503",color: "green",}}
             key={tool.name}
         >
 
             {/* THIS IS THE TOOL NAME */}
-            <span style={{ color: tool.name === selectedTool ? "white" : "red", marginright: "15px" }}>
+            <span style={{ color: tool.name === selectedTool ? "green" : "red", marginright: "15px" }}>
             {tool.icon}
             </span>
 
@@ -69,6 +81,19 @@ export default Component = ({ selectedTool, setSelectedTool }) => (
     
         </Box>
     </Stack>
-
-
 )
+
+export default Side_bar_MockUp;
+
+/*
+
+In React, the key attribute is used when rendering lists of elements to help React efficiently update and reorder
+ the elements. When rendering a list of items in React using a loop  or mapping, 
+ you should provide a unique key prop to each child element. 
+ This helps React identify individual elements and track their changes efficiently during re-renders.
+
+                                --- From GPT
+
+So key should be unique for each element. That's it.
+
+*/
